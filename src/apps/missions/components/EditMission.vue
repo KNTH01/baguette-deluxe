cf5c22b97adddce50f2a15554ff883e87997f249
Kevin Nguyen <kevin.tuanhuy.nguyen@gmail.com>
2020-02-28 19:38:54 +0100

    Extract input from EditMission form and add some validation

--- src/apps/missions/components/EditMission.vue
+++ src/apps/missions/components/EditMission.vue
@@ -2,15 +2,18 @@
   <div>
     <form @submit.prevent="editMission">
       <p>
-        <input
-          class="block w-full px-4 py-2 leading-normal bg-white border border-gray-300 rounded-lg appearance-none focus:outline-none focus:shadow-outline"
-          type="text"
-          v-model="missionName"
-        />
+        <EditMissionInput
+          :input-text.sync="missionName"
+          :is-valid.sync="isValid"
+        ></EditMissionInput>
       </p>
       <p>
         <button
-          class="px-4 py-2 my-4 font-bold text-white rounded bg-primary hover:opacity-75"
+          :disabled="!isValid"
+          :class="{
+            'opacity-50 cursor-not-allowed': !isValid,
+          }"
+          class="px-4 py-2 my-4 font-bold text-white rounded bg-primary hover:shadow-md"
           type="submit"
         >
           Edit mission
@@ -22,9 +25,12 @@
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import EditMissionInput from './EditMissionInput.vue';
 
-// Define the props by using Vue's canonical way.
 const EditMissionProps = Vue.extend({
+  components: {
+    EditMissionInput,
+  },
   props: {
     mission: {
       type: Object,
@@ -36,9 +42,10 @@ const EditMissionProps = Vue.extend({
 @Component
 export default class EditMission extends EditMissionProps {
   missionName = '';
+  isValid = false;
 
   async editMission(): Promise<void> {
-    if (this.mission.editable && this.missionName.trim()) {
+    if (this.mission.editable && this.isValid) {
       try {
         await this.$store.dispatch('editMission', {
           id: Number(this.$route.params.id),
---
+++ src/apps/missions/components/EditMissionInput.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <input
+      class="block w-full px-4 py-2 leading-normal bg-white border border-gray-300 rounded-lg appearance-none focus:outline-none focus:shadow-outline"
+      type="text"
+      v-model="inputTextModel"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { Watch, Component, Vue } from 'vue-property-decorator';
+
+// Define the props by using Vue's canonical way.
+const EditMissionProps = Vue.extend({
+  props: {
+    inputText: {
+      type: String,
+      required: true,
+    },
+  },
+});
+
+@Component
+export default class EditMission extends EditMissionProps {
+  inputTextModel = '';
+
+  get isValid(): boolean {
+    return this.inputTextModel.trim().length > 8;
+  }
+
+  @Watch('inputTextModel')
+  onInputTextModelChanged(value: string): void {
+    this.$emit('update:inputText', value);
+  }
+
+  @Watch('isValid')
+  onValidChange(value: string): void {
+    this.$emit('update:isValid', value);
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
