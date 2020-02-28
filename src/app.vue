a141666748cad62ec59bfa48b9b77c48a7073daa
Kevin Nguyen <kevin.tuanhuy.nguyen@gmail.com>
2020-02-28 10:50:48 +0100

    Add TheNavbar component

--- src/app.vue
+++ src/app.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
-    <nav>
-      <router-link :to="{ name: 'Home' }">Home</router-link>
-    </nav>
+    <TheNavbar></TheNavbar>
     <router-view></router-view>
   </div>
 </template>
 
 <script lang="ts">
+import TheNavbar from '@/apps/missions/components/TheNavbar.vue';
 import { Component, Vue } from 'vue-property-decorator';
 
-@Component
+@Component({
+  components: {
+    TheNavbar,
+  },
+})
 export default class App extends Vue {
   created(): void {
     console.log(process.env.VUE_APP_HELLO_DEV);
---
+++ src/apps/missions/components/TheNavbar.vue
@@ -0,0 +1,11 @@
+<template>
+  <nav>
+    <router-link :to="{ name: 'Home' }">Home</router-link>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
