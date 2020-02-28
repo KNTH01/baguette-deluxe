c44184a4e257f68185489053941e32e4e2101ba8
Kevin Nguyen <kevin.tuanhuy.nguyen@gmail.com>
2020-02-28 12:20:43 +0100

    Test the profile state

--- tests/unit/apps/missions/TheNavbar.spec.ts
+++ tests/unit/apps/missions/TheNavbar.spec.ts
@@ -1,16 +1,34 @@
 import { shallowMount, createLocalVue } from '@vue/test-utils';
 import TheNavbar from '@/apps/missions/components/TheNavbar.vue';
 import VueRouter from 'vue-router';
+import Vuex from 'vuex';
 
 const localVue = createLocalVue();
+localVue.use(Vuex);
 localVue.use(VueRouter);
+
 const router = new VueRouter();
 
 describe('TheNavbar.vue Test', () => {
+  let state;
+  let store: any;
+
+  beforeEach(() => {
+    state = {
+      profile: {
+        name: 'Dander Perro',
+      },
+    };
+
+    store = new Vuex.Store({
+      state,
+    });
+  });
+
   it('renders routes when component is created', () => {
     // render the component
-    const wrapper = shallowMount(TheNavbar, { localVue, router });
+    const wrapper = shallowMount(TheNavbar, { localVue, router, store });
 
-    expect(wrapper.text()).toMatch('Home');
+    expect(wrapper.text()).toMatch('D. Perro');
   });
 });
