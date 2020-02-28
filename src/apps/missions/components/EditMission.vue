53b881bfa992e2ef846bb3d649209aa5104f9938
Kevin Nguyen <kevin.tuanhuy.nguyen@gmail.com>
2020-02-28 16:06:51 +0100

    Prepare the EditMission form

---
+++ src/apps/missions/components/EditMission.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <form @submit.prevent="editMission">
+      <p>
+        <input type="text" v-model="missionName" />
+      </p>
+      <p>
+        <button type="submit">Edit mission</button>
+      </p>
+    </form>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import { IfcMission } from '@/store/ifc';
+
+@Component({
+  props: {
+    mission: {
+      type: Object,
+      required: true,
+    },
+  },
+})
+export default class EditMission extends Vue {
+  missionName = '';
+
+  async editMission(): Promise<void> {
+    console.log(this.missionName);
+    this.missionName = '';
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
--- src/apps/missions/views/Detail.vue
+++ src/apps/missions/views/Detail.vue
@@ -2,15 +2,23 @@
   <div>
     <h1>Detail</h1>
     {{ $route.params.id }}
-    <div v-if="mission">{{ mission.id }}{{ mission.name }}</div>
+    <div v-if="mission">
+      <p>{{ mission.id }}{{ mission.name }}</p>
+      <EditMission :mission="mission"></EditMission>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import { IfcMission } from '@/store/ifc';
+import EditMission from '@/apps/missions/components/EditMission.vue';
 
-@Component({})
+@Component({
+  components: {
+    EditMission,
+  },
+})
 export default class Detail extends Vue {
   get mission(): IfcMission | null {
     return this.$store.state.missions.find((mission: IfcMission) => {
