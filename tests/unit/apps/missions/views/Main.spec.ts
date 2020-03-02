f578946cf7fb45ff51cea6918a55de7009c77c4b
Kevin Nguyen <kevin.tuanhuy.nguyen@gmail.com>
2020-03-02 18:35:48 +0100

    Add Main view test

---
+++ tests/unit/apps/missions/views/Main.spec.ts
@@ -0,0 +1,57 @@
+import Vuex from 'vuex';
+import { createLocalVue, shallowMount } from '@vue/test-utils';
+import MainView from '@/apps/missions/views/Main.vue';
+
+const localVue = createLocalVue();
+localVue.use(Vuex);
+
+describe('MainView Test', () => {
+  let store: any;
+
+  beforeEach(() => {
+    const state = {
+      missions: [
+        {
+          id: 1,
+          name: 'Inform everyone Xiaomi offers best quality and price',
+          editable: true,
+        },
+        {
+          id: 2,
+          name: 'Austrian potatoes',
+          editable: false,
+        },
+        {
+          id: 3,
+          name: 'Discover why 42 is the answer to life',
+          editable: true,
+        },
+        {
+          id: 4,
+          name: 'Say something',
+          editable: false,
+        },
+      ],
+    };
+
+    const getters = {
+      missionCount: (state: any) => state.missions.length,
+    };
+
+    store = new Vuex.Store({
+      state,
+      getters,
+    });
+  });
+
+  it('renders main view with the mission count', () => {
+    // render the component
+    const wrapper = shallowMount(MainView, {
+      localVue,
+      store,
+    });
+
+    expect(wrapper.find('span').exists()).toBe(true);
+    expect(wrapper.find('span').text()).toMatch('4');
+  });
+});
